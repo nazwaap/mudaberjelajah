13954ce6f45aa7e1d6bd11df18008fe6a6e31e87
muda berjelajah <mudaberjelajah@example.com>
2024-06-10 20:58:07 +0700

    first commit

---
+++ wp-content/plugins/wp-travel-engine/dist/admin/wte-admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '8561a973311010b51dce');
---
+++ wp-content/plugins/wp-travel-engine/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-travel-engine/core',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5d0107e188cf3cbf17f358dac056787ce85d58a6',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hybridauth/hybridauth' => array(
+            'pretty_version' => 'v3.11.0',
+            'version' => '3.11.0.0',
+            'reference' => 'b70b9cbe1c7fa99b5847c5868610a8f024cc7d38',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hybridauth/hybridauth',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-travel-engine/core' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5d0107e188cf3cbf17f358dac056787ce85d58a6',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
